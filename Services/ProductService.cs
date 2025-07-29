7520c889c7b272383bec66f3f24a058b5a23a74e
User Name <mckylesingh71@gmail.com>
2025-07-29 18:33:29 +0200

    Created a products service

---
+++ Services/ProductService.cs
@@ -0,0 +1,34 @@
+﻿using ABC_Retail.Models;
+using Azure.Data.Tables;
+
+namespace ABC_Retail.Services
+{
+    public class ProductService
+    {
+        private readonly TableClient _table;
+
+        public ProductService(TableServiceClient serviceClient)
+        {
+            _table = serviceClient.GetTableClient("Products");
+            _table.CreateIfNotExists(); // Safe init
+        }
+
+        public async Task AddProductAsync(Product product) =>
+            await _table.AddEntityAsync(product);
+
+        public async Task<List<Product>> GetProductsAsync()
+        {
+            var items = new List<Product>();
+            await foreach (Product p in _table.QueryAsync<Product>())
+                items.Add(p);
+            return items;
+        }
+
+        public async Task UpdateProductAsync(Product product) =>
+            await _table.UpdateEntityAsync(product, product.ETag);
+
+        public async Task DeleteProductAsync(string rowKey) =>
+            await _table.DeleteEntityAsync("Retail", rowKey);
+
+    }
+}
