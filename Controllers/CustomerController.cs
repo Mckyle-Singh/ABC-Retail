481c8b35fd5cd6e585f2c38407baa094195f6132
User Name <mckylesingh71@gmail.com>
2025-08-02 14:09:22 +0200

    Created the Register controller and view for customers

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using ABC_Retail.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ABC_Retail.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly CustomerService _customerService;
+
+        public CustomerController(CustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
