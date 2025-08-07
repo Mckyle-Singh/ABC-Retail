0ea9c370eefdcf0772377175dcd7c09cf27a3c04
User Name <mckylesingh71@gmail.com>
2025-08-07 18:32:49 +0200

    Created the admin controller

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,43 @@
+﻿using ABC_Retail.Models.ViewModels;
+using ABC_Retail.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ABC_Retail.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly AdminService _adminService;
+
+        public AdminController(AdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        public IActionResult Login()
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginCustomerViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var admin = await _adminService.LoginAdminAsync(model.Email, model.Password);
+            if (admin == null)
+            {
+                ModelState.AddModelError("", "Invalid login attempt.");
+                return View(model);
+            }
+
+            HttpContext.Session.SetString("AdminEmail", admin.Email);
+            TempData["SuccessMessage"] = "Welcome, Admin!";
+            return RedirectToAction("Dashboard");
+        }
+
+
+
+    }
+}
