16a706be315293d5e5be24782f373e27e3770550
User Name <mckylesingh71@gmail.com>
2025-08-22 22:09:07 +0200

    Create a lightweight model to represent product changes in the UI

---
+++ Models/ViewModels/ProductChangeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ABC_Retail.Models.ViewModels
+{
+    public class ProductChangeViewModel
+    {
+        public string ProductId { get; set; }
+        public string ChangeType { get; set; } // e.g., "PriceUpdated", "StockDecremented"
+        public string Description { get; set; }
+        public DateTime Timestamp { get; set; }
+
+    }
+}
