46791a65f5f432db2c185f650cf3699a066a0c73
User Name <mckylesingh71@gmail.com>
2025-08-22 20:56:29 +0200

    Add Messaging folder with QueueReaderService for deserializing Azure Queue messages

---
+++ Services/Messaging/IQueueReaderService.cs
@@ -0,0 +1,9 @@
+﻿using ABC_Retail.Models.Messaging;
+
+namespace ABC_Retail.Services.Messaging
+{
+    public interface IQueueReaderService
+    {
+        Task<ProductChangeMessage?> ReadNextMessageAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Services/Messaging/QueueReaderService.cs
@@ -0,0 +1,34 @@
+﻿using ABC_Retail.Models.Messaging;
+using Azure.Storage.Queues;
+using System.Text.Json;
+
+namespace ABC_Retail.Services.Messaging
+{
+    public class QueueReaderService:IQueueReaderService
+    {
+        private readonly QueueClient _queueClient;
+
+        public QueueReaderService(QueueClient queueClient)
+        {
+            _queueClient = queueClient;
+        }
+
+        public async Task<ProductChangeMessage?> ReadNextMessageAsync(CancellationToken cancellationToken = default)
+        {
+            var response = await _queueClient.ReceiveMessageAsync(cancellationToken: cancellationToken);
+            if (response?.Value?.MessageText is null)
+                return null;
+
+            try
+            {
+                return JsonSerializer.Deserialize<ProductChangeMessage>(response.Value.MessageText);
+            }
+            catch (JsonException ex)
+            {
+                // Log and optionally dead-letter or delete malformed message
+                return null;
+            }
+        }
+
+    }
+}
