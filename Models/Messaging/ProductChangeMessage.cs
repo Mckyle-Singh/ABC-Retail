bd6b23eb210222db0c59663dae1560e746f06959
User Name <mckylesingh71@gmail.com>
2025-08-22 20:49:24 +0200

    Add ProductChangeMessage model to Messaging folder for decoding Azure Queue payloads

---
+++ Models/Messaging/ProductChangeMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace ABC_Retail.Models.Messaging
+{
+    public class ProductChangeMessage
+    {
+        public string ProductName { get; set; }
+        public string ChangeType { get; set; } // "Create", "Update", "Delete"
+        public DateTime Timestamp { get; set; }
+        public string Details { get; set; } // Optional: deltas, notes
+
+    }
+}
