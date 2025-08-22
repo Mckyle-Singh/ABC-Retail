a01aa5f39bc57b5f170fd3547314568843a76340
User Name <mckylesingh71@gmail.com>
2025-08-22 14:22:03 +0200

    Added the ProductChangeMessageDto for new product queue

---
+++ Models/DTOs/ProductChangeMessageDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ABC_Retail.Models.DTOs
+{
+    public class ProductChangeMessageDto
+    {
+        public string ProductId { get; set; }
+        public string Action { get; set; } // "Created", "Updated", "Deleted"
+        public Dictionary<string, (string OldValue, string NewValue)>? Changes { get; set; }
+        public string ChangedBy { get; set; }
+        public DateTime Timestamp { get; set; }
+
+    }
+}
