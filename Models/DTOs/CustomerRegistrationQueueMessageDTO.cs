9bc5bf00c167128006d68841d04908530854e35e
User Name <mckylesingh71@gmail.com>
2025-09-28 19:32:21 +0200

    Define a simple DTO that represents the registration data sent through the customer-registration-queue

---
+++ Models/DTOs/CustomerRegistrationQueueMessageDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ABC_Retail.Models.DTOs
+{
+    public class CustomerRegistrationQueueMessageDTO
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime RegisteredOn { get; set; }
+    }
+}
